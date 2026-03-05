d39c64a670b268f14300f387b95f73d6115563de
ATC <tennissg@web.de>
2026-03-05 03:31:56 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -51,5 +51,5 @@ function sendWhatsApp() {
                     `🤝 Zeuge: ${zeuge}`;
 
     // WhatsApp Link öffnen (deine Nummer)
-    window.open(`https://wa.me{message}`, '_blank');
+    window.open(`https://wa.me/491775211552 ?text=${message}`, '_blank');
 }
